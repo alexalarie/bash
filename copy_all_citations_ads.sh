39b6db02e6408f7b0d117839f125abdeeaf559d1
alexalarie <alexandre.alarie@gmail.com>
2017-10-31 10:50:53 -0500

    Create copy_all_citations_ads.sh

---
+++ copy_all_citations_ads.sh
@@ -0,0 +1,17 @@
+# Download all linked papers from ADS url
+
+url="http://adsabs.harvard.edu/cgi-bin/nph-ref_query?bibcode=2008ApJS..178...20A&amp;refs=CITATIONS&amp;db_key=AST"
+
+links=$(wget --quiet -O - $url | grep -o 'http://[^"]*')
+
+for link in $links
+do
+    # Look for the ABSTRACT URL
+    if echo "$link" | grep -q "link_type=ARTICLE"; then
+        url=$(echo $link | sed -e "s/#38;//" | sed -e "s/#38;//")
+        # Get the filename
+        filename=$(echo $url | sed -e "s/http:\/\/adsabs.harvard.edu\/cgi-bin\/nph-data_query?bibcode=\(.*\)&db_key=AST&link_type=ARTICLE/\1/" | tr . _)
+        # Download the file
+        wget --tries=3 $url -O $filename'.pdf'
+    fi
+done
