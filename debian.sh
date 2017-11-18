1f31f886372727412340052e52bd5171eed202e1
alexalarie <alexandre.alarie@gmail.com>
2017-11-18 17:10:50 -0600

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,5 +1,8 @@
 # Give read/write permissions to a disk
 chown -R alex:alex /media/alex/sandiska
 
-# Delete all soft-link recursively in a folder
+# Delete all soft-link recursively from a root folder
 find -type l -delete
+
+# Delete all given file recursively from a root folder
+find . -name "pymap_pc_up0001.nfn" -print0 | xargs -0 rm -rf
