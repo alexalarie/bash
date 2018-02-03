5dd8883e4dc3e6cecf7ae9879c42c0961c05198a
alexalarie <alexandre.alarie@gmail.com>
2018-02-03 11:41:04 -0600

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -3,3 +3,12 @@ grep sshd.\*Failed /var/log/auth.log
 
 # Look for failed connections
 grep sshd.*Did /var/log/auth.log
+
+# SSH login without password
+ssh-keygen -t rsa # Press enter to all
+# Create .ssh file on target
+cat .ssh/id_rsa.pub | ssh b@B 'cat >> .ssh/authorized_keys'
+
+# Change permission to folder/file
+.ssh to 700
+.ssh/authorized_keys to 640
