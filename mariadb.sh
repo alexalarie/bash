2640eb21a4af443e95e1da8ba182992b560c265e
alexalarie <alexandre.alarie@gmail.com>
2018-06-11 16:01:16 -0500

    Update mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -1,3 +1,4 @@
+# Install Mariadb on Ubuntu 18.04 / Debian 9
 ##########################################################################
 $ sudo apt update
 $ sudo apt install mariadb-server mariadb-client
@@ -12,4 +13,6 @@ mysql> exit;
 
 $ service mysql restart
 ##########################################################################
-
+# Create a new user and grant all privileges
+CREATE USER 'OVN_admin'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON * . * TO 'OVN_admin'@'localhost';
